51f9d19cffd5997488eed23e0c9c3430dd31f32b
thestrongchin <riverchen0510@gmail.com>
2024-09-21 15:07:08 -0700

    Lecture 2
    lecture 2 notes

---
+++ main.c
@@ -0,0 +1,21 @@
+
+// #include <stdio.h>
+// int main() {
+//     float num1 = 3.141596 ;
+//     int num2 = 2 ;
+//     printf("Number 1: %f, Number 2: %d\n", num1 , num2);
+//     printf("Vincent is a triangle " );
+//     printf("Jack is an oval  \n");
+//     printf("Trey is tall  \n");
+//     printf("Justin is short  \n");
+//     printf("Evan is kinda chunky  \n");
+//     return 0;
+// }
+#include <stdio.h>
+int main () {
+    int int_var = 123;
+    int_var = 321;
+    printf ("number: %d ", int_var);
+    return 0;
+}
+    
